6e7d241812b600c0ae27067530157db813bedc71
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-28 23:39:55 +0100

    edited app container

--- app/src/main/java/com/example/marsphotos/data/AppContainer.kt
+++ app/src/main/java/com/example/marsphotos/data/AppContainer.kt
@@ -4,7 +4,6 @@ import com.example.marsphotos.network.MarsApiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
